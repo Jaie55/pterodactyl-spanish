612efb37eb3828534fa1e6341d3bb6721bcb0188
Jon Ander <jon-1200@hotmail.es>
2024-03-03 08:31:25 +0100

    Delete resources/lang directory

--- resources/lang/de/dashboard/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'search' => 'Server suchen...',
-    'no_matches' => 'Es wurden keine Server gefunden, die den angegebenen Suchkriterien entsprechen.',
-    'cpu_title' => 'CPU',
-    'memory_title' => 'RAM',
-];
